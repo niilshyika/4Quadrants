92233c2474c511a3fd35e4c1efb4fb8af3a9a4d3
niilshyika <skateshyika@gmail.com>
2017-11-22 00:51:11 +0300

    add buttons

---
+++ README.md
@@ -0,0 +1,12 @@
+# 4Quadrants
+
+This chrome extension can help to prioritize your task
+
+## Features
+
+- Warn you about important task if you enter some url's
+- You can drag'n'drop task to change it's status.
+
+## Details
+
+- Uses dragula.js (https://github.com/bevacqua/dragula).
--- index.js
+++ index.js
@@ -3,64 +3,60 @@
 </li>*/}
 
 let tasks = [
-    {
-        id: 0,
-        text: 'Заниматься JS',
-        status: 1
-    },
-    {
-        id: 1,
-        text: 'Web Purple ( create speaker modal )',
-        status: 0
-    },
-    {
-        id: 2,
-        text: 'Issues ( work )',
-        status: 0
-    },
-    {
-        id: 3,
-        text: '"The nature of code"',
-        status: 1
-    },
-    {
-        id: 4,
-        text: 'Waste time in the VK',
-        status: 1
-    },
+  {
+    id: 0,
+    text: 'Заниматься JS',
+    type: 1
+  },
+  {
+    id: 1,
+    text: 'Web Purple (create speaker modal)',
+    type: 0
+  },
+  {
+    id: 2,
+    text: 'Issues ( work )',
+    type: 0
+  },
+  {
+    id: 3,
+    text: '"The nature of code"',
+    type: 1
+  },
+  {
+    id: 4,
+    text: 'Waste time in the VK',
+    type: 1
+  },
 ];
 
-let containers = [
-    document.querySelector('#I'),
-    document.querySelector('#II'),
-    document.querySelector('#III'),
-    document.querySelector('#IV')
+const containers = [
+  document.querySelector('#I'),
+  document.querySelector('#II'),
+  document.querySelector('#III'),
+  document.querySelector('#IV')
 ];
 
-function renderTasks() {
-    tasks.forEach((el) => {
-        let taskCont = document.createElement('li');
-        let taskSpan = document.createElement('span');
-        taskSpan.className = 'task';
-        taskSpan.innerText = el.text;
-        taskCont.appendChild(taskSpan);
-        containers[el.status].appendChild(taskCont);
-    });
-}
+function init() {
+  setLocalStorage();
+  renderTasks();
+  Array.from(document.getElementsByClassName('add-button'))
+    .forEach(button => button.addEventListener('click', e => {
+      console.log(e.target.parent);
+    }))
 
-renderTasks();
-
-dragula(containers, {
+  dragula(containers, {
     // isContainer: function(el){
     //     console.log(el);
     // },
+    removeOnSpill: true,
     accepts: function (el, target, source, sibling) {
-        // console.log(el);
-        console.log(target);
-        // console.log(source);
-        // console.log(sibling);
+      // console.log(el);
+      // console.log(target);
+      // console.log(source);
+      // console.log(sibling);
 
-        return true; // elements can be dropped in any of the `containers` by default
+      return true; // elements can be dropped in any of the `containers` by default
     },
     // moves: function (el, source, handle, sibling) {
     // console.log(source);
@@ -69,4 +65,37 @@ dragula(containers, {
 
     // return true; 
     //   },
-});
+  });
+}
+
+function setLocalStorage() {
+  window.localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function getTasks() {
+  return JSON.parse(window.localStorage.getItem('tasks')) || [];
+}
+
+function makeTaskNode(obj) {
+  let taskCont = document.createElement('li');
+  let taskSpan = document.createElement('span')
+  taskSpan.className = 'task';
+  taskSpan.innerText = obj.text;
+  taskCont.appendChild(taskSpan);
+
+  return taskCont;
+}
+
+function renderTasks() {
+  let tasks = getTasks();
+  !tasks.length || tasks.forEach(task => {
+    containers[task.type].appendChild(makeTaskNode(task));
+  });
+}
+
+function addTask(e) {
+  // tasks.push()
+  alert(e.target);
+}
+
+init();
